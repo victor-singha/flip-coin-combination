9c33ec13bb4e8d369a1cee25b04b97433060acd5
victor-singha <vicunite2@gmail.com>
2021-04-08 13:22:10 +0530

    singlet function added

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,8 +1,23 @@
 #! /bin/bash
-result=$((RANDOM%2))
-if [ $result -eq 0 ]
-then
-	echo "head"
-else
-	echo "tail"
-fi
+function singlet(){
+	result=$((RANDOM%2))
+	if [ $result -eq 0 ]
+	then
+		echo "H"
+	else
+		echo "T"
+	fi
+}
+declare -A occurence
+declare -A percentage
+occurence=([H]=0 [T]=0)
+for((i=1;i<=100;i++))
+do
+	res=$(singlet)
+	echo $res
+	occurence[$res]=$((${occurence[$res]}+1))
+done
+echo ${occurence[@]}
+percentage[H]=$((${occurence[H]}/50*100))
+percentage[T]=$((${occurence[T]}/50*100))
+echo ${percentage[@]}
